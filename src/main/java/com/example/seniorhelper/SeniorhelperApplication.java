6114ab3089c5302a627d163f6e5c809fce9b8b79
김재웅 <bugs10613@gmail.com>
2025-11-16 22:46:10 +0900

    Initial commit

---
+++ src/main/java/com/example/seniorhelper/SeniorhelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.seniorhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeniorhelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seniorhelper/SeniorhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seniorhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorhelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
